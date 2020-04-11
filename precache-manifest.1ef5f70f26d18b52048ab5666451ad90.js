7e7338357b160d1bcab7c3e8152742fe8264169b
MohamedOsama26 <you@example.com>
2020-04-11 02:24:23 +0200

    Updates

---
+++ precache-manifest.1ef5f70f26d18b52048ab5666451ad90.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0ef35772af6ab233e95d1f8c279861",
+    "url": "/outside/index.html"
+  },
+  {
+    "revision": "7fb30ad51eedca3d8adb",
+    "url": "/outside/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "16164920c6ea87739876",
+    "url": "/outside/static/css/main.219a3c7d.chunk.css"
+  },
+  {
+    "revision": "7fb30ad51eedca3d8adb",
+    "url": "/outside/static/js/2.6483a5a3.chunk.js"
+  },
+  {
+    "revision": "2304438d35f73a12e873d5d35499cd45",
+    "url": "/outside/static/js/2.6483a5a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16164920c6ea87739876",
+    "url": "/outside/static/js/main.64a2f840.chunk.js"
+  },
+  {
+    "revision": "e3f7dbbf24493babdcdf",
+    "url": "/outside/static/js/runtime-main.26bb66fa.js"
+  },
+  {
+    "revision": "e85e2199fce6ecedefcd51862cf7be4b",
+    "url": "/outside/static/media/colorsPNG.e85e2199.png"
+  },
+  {
+    "revision": "585ac8f1c73a320a7f551e1679f72b06",
+    "url": "/outside/static/media/girlBar.585ac8f1.jpg"
+  },
+  {
+    "revision": "9853af268470b2d053d1ab862643db94",
+    "url": "/outside/static/media/imageBar1.9853af26.jpg"
+  },
+  {
+    "revision": "1e03d61a25f1d7c0c35f4e7b42884147",
+    "url": "/outside/static/media/men.1e03d61a.jpg"
+  },
+  {
+    "revision": "de31f1dc216b9b49f0236df8f85121cf",
+    "url": "/outside/static/media/openShow.de31f1dc.png"
+  },
+  {
+    "revision": "ec86307ef62de29c42777f34c218050a",
+    "url": "/outside/static/media/viewCard1.ec86307e.jpg"
+  },
+  {
+    "revision": "297efce1606dd531115ee5e84679a9b2",
+    "url": "/outside/static/media/viewCard2.297efce1.jpg"
+  }
+]);
